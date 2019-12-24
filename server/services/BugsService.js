ff143f40d9adf935fe651011aab8bc09c232273b
MadisonIngraham <madi.ingraham@gmail.com>
2019-12-24 15:23:40 -0700

    server side complete

--- server/services/BugsService.js
+++ server/services/BugsService.js
@@ -1,32 +1,37 @@
-import mongoose from 'mongoose'
-import Bug from '../models/Bug'
+import mongoose from "mongoose";
+import Bug from "../models/Bug";
 
-const _repository = mongoose.model('Bug', Bug)
+const _repository = mongoose.model("Bug", Bug);
 
 class BugsService {
-  async getAll () {
-    return await _repository.find({})
+  async getAll() {
+    return await _repository.find({});
   }
 
-  async createBug (rawData) {
-    return await _repository.create(rawData)
+  async createBug(rawData) {
+    return await _repository.create(rawData);
   }
 
-  async getById (id) {
-    return await _repository.findById(id)
+  async getById(id) {
+    return await _repository.findById(id);
   }
 
-  async editBugById (id, update) {
-    let data = await _repository.findOneAndUpdate({ _id: id }, update, {
-      new: true
-    })
-    return data
+  async editBugById(id, update) {
+    let data = await _repository.findOneAndUpdate(
+      { _id: id },
+      { closed: false },
+      update
+    );
+    return data;
   }
 
-  async closeBug (id) {
-    let data = await _repository.findOneAndUpdate({ _id: id }, { closed: true })
+  async closeBug(id) {
+    let data = await _repository.findOneAndUpdate(
+      { _id: id },
+      { closed: true }
+    );
   }
 }
 
-const bugsService = new BugsService()
-export default bugsService
+const bugsService = new BugsService();
+export default bugsService;
